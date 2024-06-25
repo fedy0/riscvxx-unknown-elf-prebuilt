dc85f5013bfaf87f3118bba4ae5646412f455bb3
Ifediora E C <ifedioraelochukwuc@gmail.com>
2024-06-25 20:20:42 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,10 +5,12 @@ set -e
 mkdir build
 export BUILD_DIR=${PWD}/build
 export PATH=${BUILD_DIR}:$PATH
-sudo apt-get install autoconf automake autotools-dev curl python3 python3-pip libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev libexpat-dev ninja-build git cmake libglib2.0-dev libslirp-dev
+sudo apt-get install -y autoconf automake autotools-dev curl python3 python3-pip libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev libexpat-dev ninja-build git cmake libglib2.0-dev libslirp-dev tar
 git clone https://github.com/riscv/riscv-gnu-toolchain
 cd riscv-gnu-toolchain
 ./configure --prefix=${BUILD_DIR} --enable-multilib
 make
 
 riscv64-unknown-elf-gcc -print-multi-lib
+cd $BUILD_DIR
+tar -czvf riscvxx-unknown-elf-prebuilt.tar.gz 
