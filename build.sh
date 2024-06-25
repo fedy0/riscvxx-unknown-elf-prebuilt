6cb8bc56fb5e9a36b75341ad973e9973e71c9ba9
Ifediora E C <ifedioraelochukwuc@gmail.com>
2024-06-25 20:01:32 +0200

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e 
+
+mkdir build
+export BUILD_DIR=${PWD}/build
+export PATH=${BUILD_DIR}:$PATH
+sudo apt-get install autoconf automake autotools-dev curl python3 python3-pip libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev libexpat-dev ninja-build git cmake libglib2.0-dev libslirp-dev
+git clone https://github.com/riscv/riscv-gnu-toolchain
+cd riscv-gnu-toolchain
+./configure --prefix=${BUILD_DIR} --enable-multilib
+make
+
+riscv64-unknown-elf-gcc -print-multi-lib
